aad8140f017b7e5d58ee9bd9ae43a9075c21bb1c
Josh Butikofer <butikofe@adobe.com>
2012-10-10 10:30:43 -0600

    Added IP address print out.

--- miniwebserver.py
+++ miniwebserver.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # If necessary, replace above with your path to python
 
+import socket
 import SimpleHTTPServer
 
+addrInfo = socket.gethostbyaddr(socket.gethostbyname(socket.gethostname()))
+ipAddr = addrInfo[2][0]
+
+print "http://" + ipAddr + ":8000/"
 SimpleHTTPServer.test()
