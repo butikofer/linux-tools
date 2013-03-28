de0cdbe473c4ac61121540a1220613d2db7012c7
Josh Butikofer <butikofe@adobe.com>
2013-03-28 15:55:08 -0700

    Fixed a bug with vim plugin setup.

--- setup.sh
+++ setup.sh
@@ -3,12 +3,11 @@
 rm ~/.vimrc
 rm ~/.screenrc 
 rm ~/.gitconfig
-rm ~/.vim
 
 # Create the proper symlinks
 ln -s `pwd`/vimrc ~/.vimrc
 ln -s `pwd`/screenrc ~/.screenrc
 ln -s `pwd`/gitconfig ~/.gitconfig
-ln -s `pwd`/vim ~/.vim
 
 cp -a bin/* ~/bin/
+cp -a vim/* ~/.vim/
